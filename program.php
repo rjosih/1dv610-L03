0a8400b76d208535fd041c94f7f26da40544f670
Rosa Ekström <rjosih@hotmail.com>
2017-09-08 10:44:15 +0200

    time stamp

--- program.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-
-echo "<html><h1>Welcome</h1></html>";
-
-
-echo "<html>Username: <input='text'></input></html><br>";
-echo "<html>Password: <input='text'></input></html>";
--- view/DateTimeView.php
+++ view/DateTimeView.php
@@ -5,8 +5,17 @@ class DateTimeView {
 
 	public function show() {
 
-		$timeString = 'TODO, Write servertime here...';
+		$weekendDay = date('l');
+		$dateNumber = date('j');
+		$monthNumber = date('F');
+		$year = date('Y');
+		$time = date('G:i:s');
+		
+		$timeString = $weekendDay .', '. 'the ' . $dateNumber .'th '.  $monthNumber .' '. $year . ', The time is ' . $time;
 
 		return '<p>' . $timeString . '</p>';
 	}
 }
+
+
+// 'Friday, the 8th of September 2017, The time is '
