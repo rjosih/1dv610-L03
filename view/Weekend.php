a880f4b113aa77957b5ed262446e5bdfd0d32aef
Rosa Ekström <rjosih@hotmail.com>
2017-10-12 12:34:40 +0200

    extra function

--- view/Weekend.php
+++ view/Weekend.php
@@ -4,17 +4,30 @@ class Weekend
 {
     public function getTime()
     {
-        $weekendDay = date('l'); // veckodag
-        if($weekendDay == 'Friday' || $weekendDay == 'Saturday' || $weekendDay == 'Sunday')
+        $weekendDay = date('N');
+        $hours = date('G'); 
+        $minutes = date('i');
+        $seconds = date('s');
+
+        $dayDifference = 5-$weekendDay;
+        $hoursDifference = 11 - $hours;
+        $minuteDifference = 60 - $minutes;
+        $secondDifference = 60 -$seconds;
+        
+        if($weekendDay == '5' && $hours > 17 || $weekendDay == '6' || $weekendDay == '7')
         {
-            echo "WEEKEND";
+            echo "ENJOY WEEKEND";
         }
         else
         {
-            echo "Not weekend";
+            echo "It is " 
+            . $dayDifference  . " days, " 
+            . $hoursDifference . " hours, " 
+            . $minuteDifference . " minutes and " 
+            . $secondDifference . " seconds left to the weekend 
+            (Friday 5pm)";
         }
         
     }
-    // echo "sda";
 }
 
